413ca1db73d40389df98e1063c1e08e35332920b
demosjarco <demosjarco@users.noreply.github.com>
2019-03-08 18:30:18 -0700

    Reverse order to make it easier to select latest version

--- main.js
+++ main.js
@@ -127,6 +127,7 @@ function updateOverviewerVersions() {
 					}
 				});
 			});
+			mainMenuTemplate[1].submenu[2].submenu.reverse();
 			Menu.setApplicationMenu(Menu.buildFromTemplate(mainMenuTemplate));
 		}
 	});
