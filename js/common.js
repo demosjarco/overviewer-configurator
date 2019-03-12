680e2f9cba8c87f05f05fd4bef17488680b3de6a
demosjarco <demosjarco@users.noreply.github.com>
2019-03-12 14:58:47 -0700

    Reverted jquery init code movement

--- js/common.js
+++ js/common.js
@@ -1,5 +1,3 @@
-window.$ = window.jQuery = require('jquery');
-
 $('aside ul li').height($(window).width() * 0.05);
 $('aside ul li').css('line-height', ($(window).width() * 0.05) + 'px');
 $(window).on('resize', function () {
