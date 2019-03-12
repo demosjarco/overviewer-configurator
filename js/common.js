22eb3fb795e6baa1054cf3a43bed9d13de547c01
demosjarco <demosjarco@users.noreply.github.com>
2019-03-12 14:55:36 -0700

    Moved common code to common.js

---
+++ js/common.js
@@ -0,0 +1,7 @@
+$('aside ul li').height($(window).width() * 0.05);
+$('aside ul li').css('line-height', ($(window).width() * 0.05) + 'px');
+$(window).on('resize', function () {
+	$('aside ul li').height($(this).width() * 0.05);
+	$('aside ul li').css('line-height', ($(this).width() * 0.05) + 'px');
+}).resize();
+$("#tabs").tabs();
