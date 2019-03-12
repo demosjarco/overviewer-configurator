7bafd3207d6d9f5eac563b6a168e3d395b7c6a0d
demosjarco <demosjarco@users.noreply.github.com>
2019-03-12 13:41:38 -0700

    Process to install overviewer

--- main.js
+++ main.js
@@ -197,6 +197,60 @@ function updateOverviewerVersions(latestVersionCallback) {
 	});
 }
 
+const AdmZip = require('adm-zip');
+const rimraf = require('rimraf');
 function updateOverviewer(link) {
-	console.log(link);
+	mainWindow.setProgressBar(0, { mode: 'indeterminate' });
+	messageLog('Checking for old overviewer version');
+	fs.readdir(app.getPath('userData'), function (err, files) {
+		if (err) throw err;
+
+		let exists = false;
+		files.forEach(function (fileName) {
+			const overviewerFolderReg = /(?<=overviewer-)\d+\.\d+\.\d+(?!.zip)$/;
+			if (overviewerFolderReg.test(fileName)) {
+				exists = true;
+				messageLog('Deleting old overviewer version');
+				rimraf(app.getPath('userData').replace(/\\/g, "/") + '/' + fileName + '/', function (err2) {
+					if (err2) throw err2;
+
+					messageLog('Deleted old overviewer version');
+					beginDownload();
+				});
+			}
+		});
+		if (!exists) {
+			messageLog('No old version detected');
+			beginDownload();
+		}
+	});
+	function beginDownload() {
+		messageLog('Downloading overviewer zip');
+		const fileNameReg = /(?<=htt(p:|ps:)\/\/overviewer.org\/builds\/win64\/\d+\/)overviewer-\d+\.\d+\.\d+\.\w+$/;
+		let fileSize = 0;
+		let downloadedSize = 0;
+		request(link).on('response', function (response) {
+			fileSize = parseInt(response.headers['content-length']);
+		}).on('data', function (chunk) {
+			downloadedSize += parseInt(chunk.length);
+			mainWindow.setProgressBar(downloadedSize / fileSize, { mode: 'normal' });
+		}).on('close', function () {
+			messageLog('Downloaded overviewer zip');
+			mainWindow.setProgressBar(1, { mode: 'indeterminate' });
+			let zip = new AdmZip(app.getPath('userData').replace(/\\/g, "/") + '/' + fileNameReg.exec(link)[0]);
+			zip.extractAllTo(app.getPath('userData').replace(/\\/g, "/") + '/', true);
+			messageLog('Extracted overviewer zip');
+			fs.unlink(app.getPath('userData').replace(/\\/g, "/") + '/' + fileNameReg.exec(link)[0], (err) => {
+				if (err) throw err;
+				messageLog('Deleted overviewer zip');
+				mainWindow.setProgressBar(1, { mode: 'none' });
+			});
+			updateLocalOverviewerVersion(function (currentVersion) {
+				mainWindow.webContents.send('gotOverviewerVersion', currentVersion);
+			});
+			updateOverviewerVersions(function (latestVersion) {
+				mainWindow.webContents.send('gotLatestOverviewerVersion', latestVersion);
+			});
+		}).pipe(fs.createWriteStream(app.getPath('userData').replace(/\\/g, "/") + '/' + fileNameReg.exec(link)[0]));
+	}
 }
