87d815046469f5ca2dfdbbe8ea8b439b621c476d
demosjarco <demosjarco@users.noreply.github.com>
2019-03-12 14:53:22 -0700

    Moved overviewer tab code in another file

---
+++ js/overviewerTab.js
@@ -0,0 +1,54 @@
+$("#tabs1-content").tabs();
+ipcRenderer.send('getOverviewerVersion');
+ipcRenderer.on('gotOverviewerVersion', function (event, version) {
+	$('span#ovVersion').text(version);
+});
+ipcRenderer.send('getLatestOverviewerVersion');
+ipcRenderer.on('gotLatestOverviewerVersion', function (event, version) {
+	$('span#latestOvVersion').text(version);
+	$('span#latestOvVersion').css('color', '#a5d6a7');
+	if ($('span#ovVersion').text() == $('span#latestOvVersion').text()) {
+		$('span#ovVersion').css('color', '#a5d6a7');
+	} else {
+		$('span#ovVersion').css('color', '#ef9a9a');
+	}
+});
+
+const si = require('systeminformation');
+setInterval(function () {
+	si.cpuCurrentspeed().then(data => {
+		$('div#cpuHistory div#cpu5 div.progressBarInside').css('height', $('div#cpuHistory div#cpu4 div.progressBarInside').css('height'));
+		$('div#cpuHistory div#cpu4 div.progressBarInside').css('height', $('div#cpuHistory div#cpu3 div.progressBarInside').css('height'));
+		$('div#cpuHistory div#cpu3 div.progressBarInside').css('height', $('div#cpuHistory div#cpu2 div.progressBarInside').css('height'));
+		$('div#cpuHistory div#cpu2 div.progressBarInside').css('height', $('div#cpuHistory div#cpu1 div.progressBarInside').css('height'));
+		$('div#cpuHistory div#cpu1 div.progressBarInside').css('height', data.avg + '%');
+	});
+
+	si.mem().then(data => {
+		$('div#ramHistory div#ram5 div.progressBarInside').css('height', $('div#ramHistory div#ram4 div.progressBarInside').css('height'));
+		$('div#ramHistory div#ram4 div.progressBarInside').css('height', $('div#ramHistory div#ram3 div.progressBarInside').css('height'));
+		$('div#ramHistory div#ram3 div.progressBarInside').css('height', $('div#ramHistory div#ram2 div.progressBarInside').css('height'));
+		$('div#ramHistory div#ram2 div.progressBarInside').css('height', $('div#ramHistory div#ram1 div.progressBarInside').css('height'));
+		$('div#ramHistory div#ram1 div.progressBarInside').css('height', (data.active / data.total) * 100 + '%');
+	});
+}, 1000);
+
+let numVisibleMessages = 25;
+
+function changeLogCount(newNumVisMessages) {
+	numVisibleMessages = newNumVisMessages;
+	if ($('#tabs1-2 ul li').length > numVisibleMessages)
+		$('#tabs1-2 ul li').slice(numVisibleMessages - 1).remove();
+}
+
+ipcRenderer.on('visualLog', function (event, message) {
+	newVisualLog(message);
+});
+
+function newVisualLog(message) {
+	let tempDate = new Date();
+	$('#tabs1-2 ul').prepend('<li><span class="timeStamp">' + tempDate.toLocaleTimeString() + '</span><span class="message">' + message + '</span></li>');
+	if ($('#tabs1-2 ul li').length > numVisibleMessages)
+		$('#tabs1-2 ul li').slice(numVisibleMessages - 1).remove();
+}
+newVisualLog('Last ' + numVisibleMessages + ' messages will appear here');
