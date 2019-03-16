87fab6cd2c739ec8d3a8d50139db0c03be0d55e4
demosjarco <demosjarco@users.noreply.github.com>
2019-03-15 20:45:52 -0700

    Apply css after ready() and change value from 5% to 3.75%

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
 const { ipcRenderer } = require('electron');
 
-$('aside ul li').height($(window).width() * 0.05);
-$('aside ul li').css('line-height', ($(window).width() * 0.05) + 'px');
-$(window).on('resize', function () {
-	$('aside ul li').height($(this).width() * 0.05);
-	$('aside ul li').css('line-height', ($(this).width() * 0.05) + 'px');
-}).resize();
 $(document).ready(function () {
 	$("#tabs").tabs();
+	$('aside ul li').height($(window).width() * 0.0375);
+	$('aside ul li').css('line-height', ($(window).width() * 0.0375) + 'px');
 });
+$(window).on('resize', function () {
+	$('aside ul li').height($(this).width() * 0.0375);
+	$('aside ul li').css('line-height', ($(this).width() * 0.0375) + 'px');
+}).resize();
