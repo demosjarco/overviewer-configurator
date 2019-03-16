acb981057c5ebc327e698296caa3a71b4f4a3d1b
demosjarco <demosjarco@users.noreply.github.com>
2019-03-15 19:37:30 -0700

    Destroy then re-create tabs to recognize new elements added

--- js/worldManagement.js
+++ js/worldManagement.js
@@ -1,12 +1,18 @@
 let tabCounter = 2;
 
+function redoTabs() {
+	$('#tabs').tabs('destroy');
+	$('#tabs').tabs();
+}
+
 ipcRenderer.on('clearWorlds', function (event, arg) {
 	$('aside ul li.worldItem').remove();
+	redoTabs();
 	tabCounter = 2;
 });
 
 ipcRenderer.on('gotWorld', function (event, worldNickName) {
 	$('aside ul').append('<li class="worldItem"><a class="worlds" href="#tabs-' + tabCounter + '">[' + worldNickName + ']</a></li>');
-	$("#tabs").tabs();
 	tabCounter++;
+	redoTabs();
 });
