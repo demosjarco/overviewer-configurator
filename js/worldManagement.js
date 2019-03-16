cccc99a912acb23495473dc0d5823076c11a9222
demosjarco <demosjarco@users.noreply.github.com>
2019-03-15 19:28:30 -0700

    Create tab on sidebar

--- js/worldManagement.js
+++ js/worldManagement.js
@@ -1,7 +1,12 @@
+let tabCounter = 2;
+
 ipcRenderer.on('clearWorlds', function (event, arg) {
 	$('aside ul li.worldItem').remove();
+	tabCounter = 2;
 });
 
 ipcRenderer.on('gotWorld', function (event, worldNickName) {
-	
+	$('aside ul').append('<li class="worldItem"><a class="worlds" href="#tabs-' + tabCounter + '">[' + worldNickName + ']</a></li>');
+	$("#tabs").tabs();
+	tabCounter++;
 });
