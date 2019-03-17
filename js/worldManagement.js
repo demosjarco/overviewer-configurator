cdf6925fdf4fefc10796f23d57158f60aa3a6ef3
demosjarco <demosjarco@users.noreply.github.com>
2019-03-16 18:57:24 -0700

    Added directional rendering

--- js/worldManagement.js
+++ js/worldManagement.js
@@ -16,7 +16,7 @@ ipcRenderer.on('clearWorlds', function (event, arg) {
 
 ipcRenderer.on('gotWorld', function (event, worldNickName, worldName, worldPath) {
 	$('aside ul').append('<li class="worldItem"><a class="worlds" href="#tabs-' + tabCounter + '">[' + worldNickName + ']</a></li>');
-	$('main').append('<div class="worldItem" id="tabs-' + tabCounter + '"><h2>[' + worldNickName + '] ' + worldName + '</h2><table><tr><td>Path:</td><td><pre>' + worldPath + '</pre></td></tr></table><h3>Render Directions</h3><hr /><p></p><h3>Render Types</h3><hr /><p></p><h3>Rerender</h3><hr /><p></p></div>');
+	$('main').append('<div class="worldItem" id="tabs-' + tabCounter + '"><h2>[' + worldNickName + '] ' + worldName + '</h2><table><tr><td>Path:</td><td><pre>' + worldPath + '</pre></td></tr></table><h3>Render Directions</h3><hr /><p></p><span>Upper Left</span><span>Upper Right</span><span>Lower Left</span><span>Lower Right</span><h3>Render Types</h3><hr /><p></p><h3>Rerender</h3><hr /><p></p></div>');
 	tabCounter++;
 	redoTabs();
 });
