8ba46494274368b371288d9b53f8c6dd00cec823
demosjarco <demosjarco@users.noreply.github.com>
2019-03-23 14:22:05 -0700

    Set direction from UI

--- js/worldTab.js
+++ js/worldTab.js
@@ -3,8 +3,14 @@ function selectedRenderDirection(worldNickName, direction) {
 	if ($('main div.worldItem div.directions div.direction.' + direction).hasClass('selected')) {
 		// Unselect
 		$('main div.worldItem div.directions div.direction.' + direction).removeClass('selected');
+		let temp = {};
+		temp[direction] = false;
+		ipcRenderer.send('updateWorldInfo', worldNickName, temp);
 	} else {
 		// Select
 		$('main div.worldItem div.directions div.direction.' + direction).addClass('selected');
+		let temp = {};
+		temp[direction] = true;
+		ipcRenderer.send('updateWorldInfo', worldNickName, temp);
 	}
 }
