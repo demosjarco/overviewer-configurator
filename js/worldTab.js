7c408abc4c256f54ff1cf235d4b8b10a10f1dd66
demosjarco <demosjarco@users.noreply.github.com>
2019-03-23 14:07:51 -0700

    Removed logging

--- js/worldTab.js
+++ js/worldTab.js
@@ -1,11 +1,9 @@
 function selectedRenderDirection(worldNickName, direction) {
 	console.log(direction);
 	if ($('main div.worldItem div.directions div.direction.' + direction).hasClass('selected')) {
-		console.log('was selected');
 		// Unselect
 		$('main div.worldItem div.directions div.direction.' + direction).removeClass('selected');
 	} else {
-		console.log('wasnt selected');
 		// Select
 		$('main div.worldItem div.directions div.direction.' + direction).addClass('selected');
 	}
