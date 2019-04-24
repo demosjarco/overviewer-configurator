d7fa088ace7517e65ff83843dff93c3d551066b6
demosjarco <demosjarco@users.noreply.github.com>
2019-04-24 15:35:35 -0700

    Removed stray console log

--- js/worldTab.js
+++ js/worldTab.js
@@ -1,5 +1,4 @@
 function selectedRenderDirection(worldNickName, direction) {
-	console.log(direction);
 	if ($('main div.worldItem div.directions div.direction.' + direction).hasClass('selected')) {
 		// Unselect
 		$('main div.worldItem div.directions div.direction.' + direction).removeClass('selected');
