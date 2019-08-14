dcc7f368f288e8d40b07d7a448edd83e87c4d008
demosjarco <demosjarco@users.noreply.github.com>
2019-08-14 15:28:10 -0700

    Moved code out of main into separate file

--- main.js
+++ main.js
@@ -30,19 +30,6 @@ ipcMain.on('stopOverviewer', (event, runType) => {
 	}
 });
 
-const overviewerVersions = require('./nodejs/overviewerVersions.js');
-ipcMain.on('getOverviewerVersion', (event, arg) => {
-	overviewerVersions.updateLocalOverviewerVersion(function (currentVersion) {
-		event.sender.send('gotOverviewerVersion', currentVersion);
-	});
-});
-
-ipcMain.on('getLatestOverviewerVersion', (event, arg) => {
-	overviewerVersions.updateOverviewerVersions(function (latestVersion) {
-		event.sender.send('gotLatestOverviewerVersion', latestVersion);
-	});
-});
-
 const config = require('./nodejs/configFile.js');
 ipcMain.on('readOldSettings', (event, arg) => {
 	config.readOldSettings();
