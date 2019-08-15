32c120fa2f08909a39bcc7b25c206a7f0902a218
demosjarco <demosjarco@users.noreply.github.com>
2019-08-14 18:29:46 -0700

    Set currently installed version

--- js/sidebar.js
+++ js/sidebar.js
@@ -22,14 +22,19 @@ function refreshOverviewerVersions() {
 let installedVersion = '';
 ipcRenderer.on('localOverviewerVersion', function (event, currentVersion) {
 	installedVersion = currentVersion;
+	$('li#overviewer ul li div.content span.status i.material-icons:contains(cloud_done)').text('cloud_download');
 	$('li#overviewer ul li.v' + currentVersion.replace(/\./g, '_') + ' div.content span.status i.material-icons').text('cloud_done');
 });
 ipcRenderer.on('newOverviewerVersions', function (event, version, url) {
-	$('li#overviewer ul').append('<li class="v' + version.replace(/\./g, '_') + '"><div class="content"><span class="status"><i class="material-icons">' + (installedVersion == version ? 'cloud_done' : 'cloud_download') + '</i></span><span>' + version + '</span></div></li>');
+	let newVersion = $('<li class="v' + version.replace(/\./g, '_') + '"><div class="content"><span class="status"><i class="material-icons">' + (installedVersion == version ? 'cloud_done' : 'cloud_download') + '</i></span><span>' + version + '</span></div></li>');
+	$('li#overviewer ul').append(newVersion);
+	newVersion.click(function (e) {
+		e.stopPropagation();
+		ipcRenderer.send('updateOverviewerVersion', url);
+	});
 });
 ipcRenderer.on('doneOverviewerVersions', function (event, version, url) {
 	$('li#overviewer ul').find('li.loading').remove();
-	setupMenu();
 });
 ipcRenderer.on('errorOverviewerVersions', function (event, message) {
 	$('li#overviewer ul').append('<li class="error"><div class="content"><span class="status"><i class="material-icons">error</i></span><span>' + message + '</span></div></li>');
@@ -37,7 +42,7 @@ ipcRenderer.on('errorOverviewerVersions', function (event, message) {
 
 ipcRenderer.on('progressOverviewerVersion', function (event, visible, version, progress = 0.00) {
 	if (visible) {
-		$('li#overviewer ul li.v' + version.replace(/\./g, '_') + ' div.content').append('<div class="progressBar"><div class="status"></div></div>');
+		$('li#overviewer ul li.v' + version.replace(/\./g, '_') + ' div.content').not(':has(div.progressBar)').append('<div class="progressBar"><div class="status"></div></div>');
 		$('li#overviewer ul li.v' + version.replace(/\./g, '_') + ' div.content div.progressBar div.status').css('width', (progress * 100) + '%');
 	} else {
 		$('li#overviewer ul li.v' + version.replace(/\./g, '_') + ' div.content div.progressBar').remove();
