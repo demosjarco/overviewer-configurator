5d0409938f93048efcc11f6d2456b19f990eb6a7
demosjarco <demosjarco@users.noreply.github.com>
2019-08-15 14:11:32 -0700

    Change sidebar transparency on mac

--- js/common.js
+++ js/common.js
@@ -3,5 +3,12 @@
 const { ipcRenderer } = require('electron');
 
 $(function () {
-
+	macOsTheme();
 });
+
+function macOsTheme() {
+	const os = require('os');
+	if (os.platform() == 'darwin') {
+		$('aside').css('background-color', 'rgba(45,105,35,1.0)');
+	}
+}
