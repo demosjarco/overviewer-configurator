5d6c13c5fd05752533536ead19d89ff86b87203e
demosjarco <demosjarco@users.noreply.github.com>
2019-08-15 14:17:19 -0700

    Nvm MacOS look will stay in line

--- js/common.js
+++ js/common.js
@@ -3,12 +3,5 @@
 const { ipcRenderer } = require('electron');
 
 $(function () {
-	macOsTheme();
-});
 
-function macOsTheme() {
-	const os = require('os');
-	if (os.platform() == 'darwin') {
-		$('aside').css('background-color', 'rgba(45,105,35,1.0)');
-	}
-}
+});
