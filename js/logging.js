c9f72a7a9de4d3d58eeae71b8b070707fddd38b9
demosjarco <demosjarco@users.noreply.github.com>
2019-08-21 17:26:41 -0700

    No need

--- js/logging.js
+++ js/logging.js
@@ -10,7 +10,6 @@ ipcRenderer.on('visualLog', function (event, message) {
 function newVisualLog(message) {
 	let tempDate = new Date();
 	$(function () {
-		console.log(message);
 		$('div#timeline').prepend('<div class="log"><div class="time">' + tempDate.toLocaleTimeString() + '</div><div class="message">' + message + '</div></div>');
 		if ($('div#timeline div.log').length > numVisibleMessages)
 			$('div#timeline div.log').slice(numVisibleMessages - 1).remove();
