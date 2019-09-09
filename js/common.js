645d50ef55b59438603df358520c06743a9518c2
demosjarco <demosjarco@users.noreply.github.com>
2019-09-09 12:11:52 -0700

    Removed some remnants from title bar

--- js/common.js
+++ js/common.js
@@ -2,33 +2,7 @@
 
 const { ipcRenderer } = require('electron');
 const { dialog } = require('electron').remote;
-const mainWindow = require('electron').remote.getCurrentWindow();
 
 $(function () {
-	windowSizeIcon();
-});
 
-function minimize() {
-	ipcRenderer.send('minimize');
-}
-function windowSizeIcon() {
-	if (mainWindow.isMaximized()) {
-		$('li#windowSizing').find('span.mdi').addClass('mdi-window-restore');
-	} else {
-		$('li#windowSizing').find('span.mdi').addClass('mdi-window-maximize');
-	}
-	mainWindow.on('maximize', () => {
-		$('li#windowSizing').find('span.mdi').removeClass('mdi-window-maximize');
-		$('li#windowSizing').find('span.mdi').addClass('mdi-window-restore');
-	});
-	mainWindow.on('unmaximize', () => {
-		$('li#windowSizing').find('span.mdi').removeClass('mdi-window-restore');
-		$('li#windowSizing').find('span.mdi').addClass('mdi-window-maximize');
-	});
-}
-function maximize() {
-	ipcRenderer.send('maximize');
-}
-function closeWindow() {
-	ipcRenderer.send('close');
-}
+});
