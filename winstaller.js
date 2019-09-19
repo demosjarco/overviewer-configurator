4f5359db7d1ad44377416287b403d315671b3067
demosjarco <demosjarco@users.noreply.github.com>
2019-09-19 13:07:14 -0700

    code was based off old example

--- winstaller.js
+++ winstaller.js
@@ -1,14 +1,12 @@
 'use strict';
 
-module.exports = async function() {
-	try {
-		await require('electron-winstaller').createWindowsInstaller({
-			appDirectory: 'builds/packaged/Overviewer Configurator-win32-x64',
-			outputDirectory: 'builds/installers',
-			exe: 'overviewer-configurator-installer.exe'
-		});
-		console.log('It worked!');
-	} catch (e) {
-		throw e;
-	}
-}
+require('electron-winstaller').createWindowsInstaller({
+	appDirectory: 'builds/packaged/Overviewer Configurator-win32-x64',
+	outputDirectory: 'builds/installers',
+	exe: 'Overviewer Configurator.exe',
+	setupExe: 'overviewer-configurator-installer.exe',
+}).then(() => {
+	console.log('Installer created');
+}, (error) => {
+	throw error;
+});
