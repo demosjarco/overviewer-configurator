29ae4155c6ccce7ef9c4319d1a92ce6e0b93659b
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-04-21 15:57:01 -0700

    dynamic arch

--- winstaller.js
+++ winstaller.js
@@ -1,13 +1,16 @@
 'use strict';
 
+const { argv } = require('process');
+
 require('electron-winstaller').createWindowsInstaller({
-	appDirectory: 'builds/packaged/Overviewer Configurator-win32-x64',
+	appDirectory: `builds/packaged/Overviewer Configurator-win32-${argv[2]}`,
 	outputDirectory: 'builds/installers',
 	exe: 'Overviewer Configurator.exe',
-	setupExe: 'overviewer-configurator-installer.exe',
+	setupExe: `overviewer-configurator-installer-${argv[2]}.exe`,
+	// setupMsi: `overviewer-configurator-installer-${argv[2]}.msi`,
 	noMsi: true
 }).then(() => {
-	console.log('Installer created');
+	console.log(`${argv[2]} Installer created`);
 }, (error) => {
 	throw error;
 });
